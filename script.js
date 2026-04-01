d8e723883759b1abfc4f78084e670dab9eeaa672
lucasalbuquerque09 <lc.albuquerque09@gmail.com>
2026-03-31 22:53:02 -0300

    update

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-/// Elementos do DOM
+// Elementos do DOM para desktop
 const inputPassword = document.getElementById('senha');
 const showHidePassword = document.getElementById('btn-senha');
 const btnLogin = document.querySelector('.btn-menu');
 const loginForm = document.querySelector('.interface-form');
 const btnclose = document.getElementById('close');
 
-// Função para mostrar ou ocultar a senha
+
+// Função para mostrar ou ocultar a senha no campo de input
 function showHide(){
     if(inputPassword.type === 'password'){
         inputPassword.setAttribute('type' , 'text');
@@ -17,13 +18,13 @@ function showHide(){
         
 }
 
-// Função para mostrar o formulário de login
+// Função para mostrar o formulário de login ao clicar no botão de login
 function showFormLogin(){
      
     loginForm.classList.add('show-Form');
 }  
 
-// Função para ocultar o formulário de login
+// Função para ocultar o formulário de login ao clicar no botão de fechar
 function hideFormLogin(){
     loginForm.classList.add('hide-Form');
     loginForm.classList.remove('show-Form');
@@ -34,12 +35,38 @@ function hideFormLogin(){
 
 }
 
-// Eventos para desktop
+// Eventos para desktop 
 showHidePassword.addEventListener('click' , showHide);
 btnLogin.addEventListener('click' , showFormLogin);
 btnclose.addEventListener('click' , hideFormLogin);
 
-// Eventos para dispositivos móveis
-showHidePassword.addEventListener('touchstart' , showHide);
-btnLogin.addEventListener('touchstart' , showFormLogin);
-btnclose.addEventListener('touchstart' , hideFormLogin);
+
+
+// Elementos do DOM para mobile
+const btnMenuMobile = document.getElementById('open-mobile');
+const openMenuMobile = document.querySelector('.menu-mobile');
+const overlayMenu = document.getElementById('overlay')
+const linkMenu = document.getElementById('link-menu');
+
+// Função para abrir o menu mobile
+btnMenuMobile.addEventListener('click' , () =>{
+    
+    openMenuMobile.classList.add('active-menu')
+    
+})
+openMenuMobile.addEventListener('click' , () =>{
+    
+    openMenuMobile.classList.remove('active-menu')
+
+})
+overlayMenu.addEventListener('click' , () =>{
+    
+    openMenuMobile.classList.remove('active-menu')
+
+})
+
+// Função para mostrar o formulário de login ao clicar no link do menu mobile
+linkMenu.addEventListener('click' , () =>{
+    
+    loginForm.classList.add('show-Form');
+})
