296f19149209fc9c41b14d7cf82831297a73807e
lucasalbuquerque09 <lc.albuquerque09@gmail.com>
2026-04-01 22:15:41 -0300

    update

--- script.js
+++ script.js
@@ -1,72 +1,63 @@
-// Elementos do DOM para desktop
-const inputPassword = document.getElementById('senha');
-const showHidePassword = document.getElementById('btn-senha');
+// elementos do DOM para manipulação do formulário de login e registro
+const mainForm = document.querySelector('.main-form');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
 const btnLogin = document.querySelector('.btn-menu');
-const loginForm = document.querySelector('.interface-form');
-const btnclose = document.getElementById('close');
+const btnClose = document.querySelector('.icon-close');
+
+// eventos para alternar entre os formulários de login e registro
+registerLink.addEventListener('click' , ()=>{
+    mainForm.classList.add('active');
+});
+
+loginLink.addEventListener('click' , ()=>{
+    mainForm.classList.remove('active');
+});
+
+btnLogin.addEventListener('click' , ()=>{
+    mainForm.classList.add('active-main-form');
+});
+
+btnClose.addEventListener('click' , ()=>{
+    console.log('Close clicado!');
+    mainForm.classList.remove('active-main-form');
+});
 
 
-// Função para mostrar ou ocultar a senha no campo de input
-function showHide(){
-    if(inputPassword.type === 'password'){
-        inputPassword.setAttribute('type' , 'text');
-        showHidePassword.classList.replace('bi-eye-fill' , 'bi-eye-slash-fill');
+// elementos para manipulação do campo de senha
+const iconEyeLogin = document.getElementById('btn-senha-login');
+const iconEyeRegister = document.getElementById('btn-senha-register');
+const inputPass = document.getElementById('senha');
+const inputPassRegister = document.getElementById('senha-register');
+
+// eventos para alternar a visibilidade da senha nos formulários de login e registro
+iconEyeLogin.addEventListener('click' , ()=>{
+    if(inputPass.type === 'password'){
+        inputPass.setAttribute('type' , 'text');
+        iconEyeLogin.classList.replace('bi-eye-fill' , 'bi-eye-slash-fill');
     }else{
-        inputPassword.setAttribute('type' , 'password');
-        showHidePassword.classList.replace('bi-eye-slash-fill' , 'bi-eye-fill');
+        inputPass.setAttribute('type' , 'password');
+        iconEyeLogin.classList.replace( 'bi-eye-slash-fill','bi-eye-fill');
     }
-        
-}
-
-// Função para mostrar o formulário de login ao clicar no botão de login
-function showFormLogin(){
-     
-    loginForm.classList.add('show-Form');
-}  
-
-// Função para ocultar o formulário de login ao clicar no botão de fechar
-function hideFormLogin(){
-    loginForm.classList.add('hide-Form');
-    loginForm.classList.remove('show-Form');
-
-    setTimeout(() => {
-        loginForm.classList.remove('hide-Form');
-    }, 800);    
-
-}
-
-// Eventos para desktop 
-showHidePassword.addEventListener('click' , showHide);
-btnLogin.addEventListener('click' , showFormLogin);
-btnclose.addEventListener('click' , hideFormLogin);
+});
 
 
+iconEyeRegister.addEventListener('click' , ()=>{
+    if(inputPassRegister.type === 'password'){
+        inputPassRegister.setAttribute('type' , 'text');
+        iconEyeRegister.classList.replace('bi-eye-fill' , 'bi-eye-slash-fill');
+    }else{
+        inputPassRegister.setAttribute('type' , 'password');
+        iconEyeRegister.classList.replace( 'bi-eye-slash-fill','bi-eye-fill');
+    }
+});
 
-// Elementos do DOM para mobile
-const btnMenuMobile = document.getElementById('open-mobile');
-const openMenuMobile = document.querySelector('.menu-mobile');
-const overlayMenu = document.getElementById('overlay')
-const linkMenu = document.getElementById('link-menu');
-
-// Função para abrir o menu mobile
-btnMenuMobile.addEventListener('click' , () =>{
-    
-    openMenuMobile.classList.add('active-menu')
-    
-})
-openMenuMobile.addEventListener('click' , () =>{
-    
-    openMenuMobile.classList.remove('active-menu')
-
-})
-overlayMenu.addEventListener('click' , () =>{
-    
-    openMenuMobile.classList.remove('active-menu')
-
-})
-
-// Função para mostrar o formulário de login ao clicar no link do menu mobile
-linkMenu.addEventListener('click' , () =>{
-    
-    loginForm.classList.add('show-Form');
-})
+/* iconEyeRegister.addEventListener('click' , ()=>{
+    if(inputPass.type === 'password'){
+        inputPass.type = 'text'
+        iconEyeRegister.classList.replace('bi-eye-fill' , 'bi-eye-slash-fill')
+    }else{
+        inputPass.type = 'password'
+        iconEyeRegister.classList.replace( 'bi-eye-slash-fill','bi-eye-fill')
+    }
+}) */
